2f8c886e132c2d03a55d72bf179d76dda1cb3519
sanketsathe <sanketsathe81@gmail.com>
2025-07-04 18:41:00 +0530

    set routes

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,34 @@ const Mongoose=require('mongoose');
 const express=require('express');
 const Holding=require('./Model/HoldingModel');
 const Position=require('./Model/PositionModel')
-const app=express();
+const cors=require('cors');
+const BodyParser=require("body-parser");
 const url=process.env.MONGO_URL;
 const PORT=process.env.PORT||3002;
+const app=express();
+app.use(cors());
+app.use(BodyParser.json());
 async function main() {
     await Mongoose.connect(url);
 }
-
+app.get('/Holdings',async(req,res)=>{
+    try{
+        let holdings= await Holding.find({});
+        res.json(holdings);
+    }catch(err){
+        console.log(err);
+    }
+    
+})
+app.get('/Positions',async(req,res)=>{
+    try{
+        let Positions= await Position.find({});
+        res.json(Positions);
+    }catch(err){
+        console.log(err);
+    }
+    
+})
 
 app.listen(PORT,()=>{
     console.log('app started');
